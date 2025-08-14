d4daa9c8c502243dd17b79cac9108e64ef4e4a15
Nirjus <karmakarnirjus4839@gmail.com>
2025-08-15 00:12:26 +0530

    Practice the CRUD again

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,5 +1,18 @@
-import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { BookService } from './book.service';
+import { Book } from './interfaces/Book.interface';
 
 @Controller('book')
 export class BookController {
@@ -18,5 +31,33 @@ export class BookController {
   @Get('name/:id')
   getBookName(@Param('id', ParseIntPipe) id: number): string {
     return this.bookService.getBookName(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  createBook(
+    @Body()
+    bookData: Omit<Book, 'id'>,
+  ) {
+    this.bookService.create(bookData);
+  }
+
+  @Put(':id')
+  updateBook(
+    @Param('id', ParseIntPipe)
+    id: number,
+    @Body()
+    updatedBook: Partial<Omit<Book, 'id'>>,
+  ) {
+    this.bookService.updateBook(id, updatedBook);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NOT_FOUND)
+  removeBook(
+    @Param('id', ParseIntPipe)
+    id: number,
+  ) {
+    this.bookService.remove(id);
   }
 }
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,17 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { HelloService } from 'src/hello/hello.service';
+import { Book } from './interfaces/Book.interface';
 
 @Injectable()
 export class BookService {
   // constructor call to get service from hello service
   constructor(private readonly helloService: HelloService) {}
+  private books: Book[] = [
+    { name: 'Pushpa 1', id: 1, weight: 280.56 },
+    { name: 'Pushpa 2', id: 2, weight: 156.18 },
+    { name: 'Pushpa 3', id: 3, weight: 240.98 },
+    { name: 'Pushpa 4', id: 4, weight: 189.37 },
+  ];
   getAllBooks() {
-    return [
-      { name: 'Pushpa 1', id: 1, weight: 280.56 },
-      { name: 'Pushpa 2', id: 2, weight: 156.18 },
-      { name: 'Pushpa 3', id: 3, weight: 240.98 },
-      { name: 'Pushpa 4', id: 4, weight: 189.37 },
-    ];
+    return this.books;
   }
 
   getBookById(id: number) {
@@ -23,5 +25,37 @@ export class BookService {
       return 'No book found';
     }
     return this.helloService.getHelloWithName(book?.name);
+  }
+
+  create(bookData: Omit<Book, 'id'>) {
+    this.books.push({
+      id: this.increaseIndexing(),
+      ...bookData,
+    });
+  }
+
+  updateBook(id: number, bookData: Partial<Omit<Book, 'id'>>) {
+    const currentBookIndex = this.books.findIndex((book) => book.id === id);
+    if (currentBookIndex === -1) {
+      throw new NotFoundException(`This Book is not gound with id ${id}`);
+    }
+    this.books[currentBookIndex] = {
+      ...this.books[currentBookIndex],
+      ...bookData,
+    };
+  }
+
+  remove(id: number) {
+    const currentBookIndex = this.books.findIndex((book) => book.id === id);
+    if (currentBookIndex === -1) {
+      throw new NotFoundException(`This Book is not gound with id ${id}`);
+    }
+    this.books.splice(currentBookIndex, 1);
+  }
+
+  increaseIndexing(): number {
+    const currentItem = this.books[this.books.length - 1];
+    const index = this.books.length > 0 ? currentItem?.id + 1 : 1;
+    return index;
   }
 }
